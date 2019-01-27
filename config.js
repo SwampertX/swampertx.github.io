7d07bf79b392a4b5be4dc10241afb367ee560fff
Yee Jian <tanyeejian@gmail.com>
2019-01-27 21:30:49 +0800

    initial commit for gatsby

---
+++ config.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports = {
+  url: 'https://swampertx.github.io',
+  title: 'Blog by Yee Jian',
+  subtitle: 'Web developer. Aspiring ML engineer / AI researcher.',
+  copyright: '© All rights reserved.',
+  disqusShortname: '',
+  postsPerPage: 4,
+  googleAnalyticsId: 'UA-133276825-1',
+  menu: [
+    {
+      label: 'Articles',
+      path: '/'
+    },
+    {
+      label: 'About me',
+      path: '/pages/about'
+    },
+    {
+      label: 'Contact me',
+      path: '/pages/contacts'
+    }
+  ],
+  author: {
+    name: 'Yee Jian',
+    photo: '/photo.jpg',
+    bio: 'Web developer. Aspiring ML engineer / AI researcher.',
+    contacts: {
+      email: 'tanyeejian@gmail.com',
+      telegram: '#',
+      twitter: 'TanYeeJian',
+      github: 'SwampertX',
+      rss: '#',
+    }
+  }
+};
