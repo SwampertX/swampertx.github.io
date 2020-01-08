8ff965a5b038e364e34b32abfe25938bd834ef83
Tan Yee Jian <tanyeejian@gmail.com>
2020-01-08 19:06:28 +0800

    Happy New Year 2020 :D
    Made revamp to About and Projects pages.
    Removed IVLE/2040 from About to Projects.
    Restructured Projects and gave badges to them.
    Added Stepper in with a screen capture GIF.
    
    I also promised writing articles on:
    1. My first contribution on Gitlab
    2. How the stepper works.
    
    Also resized my profile photo for quicker loading.

--- config.js
+++ config.js
@@ -30,7 +30,7 @@ module.exports = {
   ],
   author: {
     name: 'Yee Jian',
-    photo: '/photo.jpg',
+    photo: '/photo_75x75.jpg',
     bio: 'Web developer. Aspiring ML engineer / AI researcher.',
     contacts: {
       email: 'tanyeejian@gmail.com',
