c20fd8172f024a1fe0b19bccb8141088e699d269
Tan Yee Jian <yeejian.tan@u.nus.edu>
2021-11-12 14:26:53 +0800

    Commit built site

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# git add .
-# git commit -m "Automated commit by deploy.sh"
-# git push origin hugo
 hugo
-git checkout master
-cp -rf public/* .
+rm -rf docs
+cp -r public docs
 git add .
-git commit -m "Automated deployment by deploy.sh"
-git push origin master
-git checkout hugo
+git commit -m "Commit built site"
+git push
